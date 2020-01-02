ef6794037025614074b39417cf9177956e61fa70
test <test@test.com.tw>
2020-01-02 10:47:37 +0800

    網站名稱 NEEDGAME -> KYOMON, 隱私權政策文字調整

--- share.php
+++ share.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>NEEDGAMES</title>
+	<title>KYOMON</title>
 	<link rel="shortcut icon" href="favicon.ico" />
 	<link href="css/global.css" type="text/css" rel="stylesheet">
     <link href="css/ui-lightness/jquery-ui-1.10.3.custom.css" type="text/css" rel="stylesheet">
