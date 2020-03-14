65921d290e259fe1ec00e3aa7d07bd29de3706f7
Lanker <test@test.com.tw>
2020-03-14 05:25:45 +0000

    簡訊王 api test html

---
+++ func/kotsmsForm.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>kotsms</title>
+        <meta charset='utf-8' />
+    </head>
+    <body>
+        <form action="https://api.kotsms.com.tw/kotsmsapi-1.php" method='GET'>
+            username<input type="text" name="username" value="iancui" />
+<br />
+            password<input type="text" name="password" value="ian81912" />
+<br />
+            dstaddr <input type="text" name="dstaddr" value="" />
+<br />
+            smbody <input type="text" name="smbody" value="test" />
+<br />
+            dlvtime <input type="text" name="dlvtime" value="0" />
+<br />
+            vldtime <input type="text" name="vldtime" value="0" />
+<br />
+            response<input type="text" name="response" value="" />
+<br />
+            <button>ok</button>
+        </form>
+    </body>
+</html>
